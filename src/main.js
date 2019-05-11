ab58b23f8d7681511248c37673b5a3025acf5d32
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-11 13:11:09 -0300

    installing vendors - flexgrid.css and normalize.css

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes'
 
+// CSS Vendors
+import 'normalize.css'
+import '@/assets/css/flexgrid.css'
+
 Vue.config.productionTip = false
 
 new Vue({

054661aa91b0600c6e13475598d36756617efe18
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-13 13:09:46 -0300

    setting colors

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,15 @@
 </template>
 
 <style>
+  :root {
+    --color-primary: #62D41C;
+    --color-white: white;
+    --color-title: #4D4D4D;
+  }
+
   body {
+    background-color: #f2f2f2;
+    color: #737373;
     font-family: 'Segoe UI', Helvetica, Arial, sans-serif;
   }
 </style>

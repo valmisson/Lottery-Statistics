c20fea45a4c921eb04a9539897c9a78b6abaf29f
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-13 17:26:14 -0300

    create header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,48 @@
 <template>
   <div id="app">
-    <router-view/>
+    <layout-header />
+
+    <router-view class="main"/>
   </div>
 </template>
 
+<script>
+import LayoutHeader from '@components/layout/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    LayoutHeader
+  }
+}
+</script>
+
 <style>
   :root {
-    --color-primary: #62D41C;
+    --color-primary: #62d41c;
+    --color-font: #737373;
     --color-white: white;
-    --color-title: #4D4D4D;
+    --color-title: #4d4d4d;
   }
 
   body {
     background-color: #f2f2f2;
-    color: #737373;
+    color: var(--color-font);
     font-family: 'Segoe UI', Helvetica, Arial, sans-serif;
+  }
+
+  /* Reset */
+  a {
+    text-decoration: none;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  @media (max-width: 768px) {
+    .main {
+      margin-top: 50px;
+    }
   }
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,179 @@
+<template>
+  <header class="layout__header">
+    <div class="container">
+      <div class="layout__header-row row">
+        <a href="/" class="layout__header-brand">Lotterias</a>
+
+        <button @click="toggleMenu" class="menu__button">
+          <span class="menu__button-cicle"></span>
+          <span class="menu__button-cicle"></span>
+          <span class="menu__button-cicle"></span>
+        </button>
+
+        <nav ref="menu" class="layout__header-menu">
+          <ul class="menu__content">
+            <li class="menu__item"><router-link to="/" class="menu__link">Estatisticas</router-link></li>
+            <li class="menu__item"><router-link to="#" class="menu__link">Resultados</router-link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+
+  methods: {
+    toggleMenu () {
+      const { menu } = this.$refs
+
+      menu.classList.toggle('isShow')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .layout__header {
+    background-color: var(--color-white);
+    box-shadow: 0 1px 4px rgba(0, 0, 0, 0.07);
+    padding-left: 15px;
+    padding-right: 15px;
+    position: fixed;
+    top: 0;
+    width: 100%;
+  }
+
+  .layout__header-row {
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    position: relative;
+  }
+
+  /* brand */
+  .layout__header-brand {
+    color: var(--color-primary);
+    font-size: 18px;
+    font-weight: 600;
+  }
+
+  /* button menu */
+  .menu__button {
+    background-color: transparent;
+    border: none;
+    cursor: pointer;
+    display: flex;
+    padding: 10px;
+  }
+
+  .menu__button:focus {
+    outline: none;
+  }
+
+  .menu__button-cicle {
+    background-color: var(--color-title);
+    border-radius: 50%;
+    display: block;
+    height: 6px;
+    margin-right: 5px;
+    width: 6px;
+  }
+
+  .menu__button-cicle:last-child {
+    margin-right: 0;
+  }
+
+  /* menu */
+  .isShow {
+    transform: translateX(-15%) !important;
+    transition: transform 0.3s ease;
+  }
+
+  .layout__header-menu {
+    background-color: var(--color-white);
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.15);
+    position: fixed;
+    padding: 15px 35px;
+    right: 0;
+    top: 50px;
+    transform: translateX(110%);
+    transition: transform 0.3s ease;
+  }
+
+  .menu__content {
+    margin: 0;
+    padding-left: 0;
+  }
+
+  .menu__item:last-child {
+    margin-top: 20px;
+  }
+
+  .menu__link {
+    color: var(--color-font);
+    font-weight: 500;
+  }
+
+  .menu__link.router-link-exact-active {
+    color: var(--color-primary);
+    position: relative;
+  }
+
+  @media (min-width: 768px) {
+    .layout__header {
+      padding: 0;
+      position: relative;
+    }
+
+    .layout__header-row {
+      height: 55px;
+    }
+
+    /* brand */
+    .layout__header-brand {
+      font-size: 20px;
+    }
+
+    /* button menu */
+    .menu__button {
+      display: none;
+    }
+
+    /* Menu */
+    .layout__header-menu {
+      background-color: transparent;
+      box-shadow: none;
+      position: relative;
+      padding: 0;
+      top: 0;
+      transform: translateX(0%);
+    }
+
+    .menu__content {
+      display: flex;
+    }
+
+    .menu__item {
+      padding-right: 50px;
+    }
+
+    .menu__item:last-child {
+      margin-top: 0;
+      padding-right: 0;
+    }
+
+    .menu__link.router-link-exact-active::after {
+      background-color: var(--color-primary);
+      content: '';
+      cursor: default;
+      display: block;
+      height: 2px;
+      position: absolute;
+      top: 36px;
+      width: 100%;
+    }
+  }
+</style>

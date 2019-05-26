d4db09459eb1bd2498d816464495d6e7d389a3f9
Valmisson Grizorte <missongrizorte@gmail.com>
2019-05-25 21:22:47 -0300

    creating error page 404

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,19 @@
 import Home from '@views/Home.vue'
 
+const PageNotFound = () => import(/* webpackChunkName: "page__404" */ '@views/PageNotFound.vue')
 const Statistics = () => import(/* webpackChunkName: "statistics" */ '@views/Statistics.vue')
-const Results = () => import(/* webpackChunkName: "statistics" */ '@views/Results.vue')
+const Results = () => import(/* webpackChunkName: "results" */ '@views/Results.vue')
 
 export default [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '*',
+    name: 'PageNotFound',
+    component: PageNotFound
   },
   {
     path: '/estatisticas/megasena',
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="page__404">
+    <h1 class="page__404-title">404</h1>
+    <h2 class="page__404-description">Página não encontrada</h2>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound'
+}
+</script>
+
+<style scoped>
+  .page__404 {
+    margin-top: 60px;
+    text-align: center;
+  }
+
+  .page__404-title {
+    color: var(--color-primary);
+    font-size: 64px;
+    margin: 0;
+  }
+
+  .page__404-description {
+    font-size: 24px;
+    margin-top: 15px;
+  }
+
+  @media (min-width: 768px) {
+    .page__404-description {
+      font-size: 32px;
+      margin-top: 20px;
+    }
+  }
+</style>
